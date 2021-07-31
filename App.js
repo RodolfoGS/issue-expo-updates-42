80a127943957742579858ed1d711f58be7e28404
Rodolfo Gomez Sirimarco <im@rodolfo.gs>
2021-07-31 11:15:05 -0500

    App.js updated

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates';
 
 export default function App() {
+  handleCheckUpdate = async () => {
+    try {
+      const asd = await Updates.fetchUpdateAsync();
+      alert(JSON.stringify(asd));
+    } catch (error) {
+      alert(JSON.stringify(error.message));
+    }
+  }
+
+  handleForceUpdate = async () => {
+    await Updates.reloadAsync();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Release: {Updates.manifest.revisionId}</Text>
+      <Button onPress={handleCheckUpdate} title="Check Update" />
+      <Text />
+      <Button onPress={handleForceUpdate} title="Force Update" />
     </View>
   );
 }
